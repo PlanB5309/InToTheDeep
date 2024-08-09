9af4970fccda7a754aec36de2fd5cfe891e6ecb4
millardgold <mgold@triad.rr.com>
2024-08-09 13:33:00 -0400

    Created DistanceToTarget and Target
    Added two classes to help with odometry
    
    Co-Authored-By: Whitmans754 <113317484+Whitmans754@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Target.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Target {
+    double x;
+    double y;
+    double h;
+    double maxSpeed;
+
+    public Target(double x, double y, double h, double maxSpeed){
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Target(){
+        x = 0;
+        y = 0;
+        h = 0;
+        maxSpeed = 0;
+    }
+}
