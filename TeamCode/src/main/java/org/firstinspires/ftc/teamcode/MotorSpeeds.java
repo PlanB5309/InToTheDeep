1038dda29704c36251a471a311a07947a6c7734c
PlanB5309 <43391798+PlanB5309@users.noreply.github.com>
2024-08-09 14:21:44 -0400

    MOVING ODOMETERY
    Made moving components that set the motor speed as well as allow the robot to actually move using how far away. It also adjusts the speed depending on how far away the robot is.
    
    Co-Authored-By: BobtheShrimp <116126182+BobtheShrimp@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorSpeeds.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorSpeeds.java
@@ -1,15 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class MotorSpeeds {
     RobotHardware robot;
+    DistanceToTarget distanceToTarget;
     double frontLeftSpeed;
     double frontRightSpeed;
     double backLeftSpeed;
     double backRightSpeed;
+    double speedFactor;
 
 
 
@@ -32,8 +35,29 @@ public class MotorSpeeds {
         this.backRightSpeed = backRightSpeed;
     }
 
+    public MotorSpeeds findMotorSpeeds (DistanceToTarget dtt, double maxSpeed){
+        double denominator;
+        denominator = Math.max(Math.abs(dtt.diffy) + Math.abs(dtt.diffx) + Math.abs(dtt.diffh), .1);
+        frontLeftSpeed = (dtt.diffy + dtt.diffx + dtt.diffh) / denominator;
+        frontRightSpeed = (dtt.diffy - dtt.diffx - dtt.diffh) / denominator;
+        backLeftSpeed = (dtt.diffy - dtt.diffx + dtt.diffh) / denominator;
+        backRightSpeed = (dtt.diffy + dtt.diffx - dtt.diffh) / denominator;
 
-    public void setMotorSpeed (MotorSpeeds motorSpeeds, double maxSpeed){
+        if (dtt.vector > 8){
+            speedFactor = maxSpeed;
+        }
+
+        else {
+            speedFactor = dtt.vector / 8 * maxSpeed;
+        }
+        frontLeftSpeed = frontLeftSpeed * speedFactor;
+        frontRightSpeed = frontRightSpeed * speedFactor;
+        backLeftSpeed = backLeftSpeed * speedFactor;
+        backRightSpeed = backRightSpeed * speedFactor;
+        return this;
+    }
+
+    public void setMotorSpeeds (MotorSpeeds motorSpeeds, double maxSpeed){
         robot.frontLeftMotor.setPower(motorSpeeds.frontLeftSpeed);
         robot.frontRightMotor.setPower(motorSpeeds.frontRightSpeed);
         robot.backLeftMotor.setPower(motorSpeeds.backLeftSpeed);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move.java
@@ -1,21 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Move {
     RobotHardware robot;
     Telemetry telemetry;
     MotorSpeeds motorSpeeds;
-//    DistanceToTarget distanceToTarget;
+    DistanceToTarget distanceToTarget;
 
     public Move (RobotHardware robot, Telemetry telemetry, MotorSpeeds motorSpeeds){
         this.robot = robot;
         this.telemetry = telemetry;
         this.motorSpeeds = motorSpeeds;
-        //this.distanceToTarget = distanceToTarget;
+        distanceToTarget = new DistanceToTarget();
     }
 
-//    public boolean moveIt ()
-
-
+    public boolean moveIt (SparkFunOTOS.Pose2D pos, Target target){
+        boolean done = false;
+        distanceToTarget = distanceToTarget.find(pos, target);
+        motorSpeeds.findMotorSpeeds(distanceToTarget, target.maxSpeed);
+        done = distanceToTarget.closeEnough(distanceToTarget);
+        if (done == true){
+            motorSpeeds.setMotorSpeeds(motorSpeeds, 0);
+        }
+        motorSpeeds.setMotorSpeeds(motorSpeeds, target.maxSpeed);
+        return false;
+    }
 }
