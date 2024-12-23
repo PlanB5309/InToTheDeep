22260efe2c702086050d40ca7a75c226d7207eb5
millardgold <mgold@triad.rr.com>
2024-12-23 10:09:20 -0500

    Update Target.java
    Syncing my laptop with the software in the shop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Target.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Target.java
@@ -4,26 +4,28 @@ public class Target {
     double x;
     double y;
     double h;
-    double maxSpeed;
+    TargetProfile tp;
 
-    public Target(double x, double y, double h, double maxSpeed){
+
+
+    public Target(double x, double y, double h, TargetProfile tp){
         this.x = x;
         this.y = y;
         this.h = h;
-        this.maxSpeed = maxSpeed;
+        this.tp = tp;
     }
 
     public Target(){
         x = 0;
         y = 0;
         h = 0;
-        maxSpeed = 0;
+
     }
 
-    public void set (double x, double y, double h, double maxSpeed){
+    public void set (double x, double y, double h){
         this.x = x;
         this.y = y;
         this.h = h;
-        this.maxSpeed = maxSpeed;
+
     }
 }
